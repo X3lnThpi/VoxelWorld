04b08e44351043b1f6081f1e73b3905b51d047d3
RADian planck <devmethyl@outlook.com>
2022-12-14 15:00:32 +0530

    World Gen

--- Assets/Scripts/Chunk.cs
+++ Assets/Scripts/Chunk.cs
@@ -2,57 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chunk : MonoBehaviour
-{
-    public Material cubeMaterial;
+public class Chunk {
 
-    IEnumerator BuildChunk(int sizeX, int sizeY, int sizeZ)
-    {
-        for(int z = 0; z < sizeZ; z++)
-            for(int y = 0; y < sizeY; y++)
-                for(int x = 0; x < sizeX; x++)
-                {
-                    Vector3 pos = new Vector3(x, y, z);
-                    Block b = new Block(Block.BlockType.DIRT, pos, this.gameObject, cubeMaterial);
-                    b.Draw();
-                    yield return null;
-                }
-        CombineQuads();
-    }
+	public Material cubeMaterial;
+	public Block[,,] chunkData;
+	public GameObject chunk;
 
-    void CombineQuads()
-    {
-        //Combine all children meshes
-        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+	void BuildChunk()
+	{
+		chunkData = new Block[World.chunkSize,World.chunkSize,World.chunkSize];
+
+		for(int z = 0; z < World.chunkSize; z++)
+			for(int y = 0; y < World.chunkSize; y++)
+				for(int x = 0; x < World.chunkSize; x++)
+				{
+					Vector3 pos = new Vector3(x,y,z);
+					if(Random.Range(0,100) < 50)
+						chunkData[x,y,z] = new Block(Block.BlockType.DIRT, pos, 
+						                chunk.gameObject, this);
+					else
+						chunkData[x,y,z] = new Block(Block.BlockType.AIR, pos, 
+						                chunk.gameObject, this);
+				}
+	}
+
+	public void DrawChunk()
+	{
+		for(int z = 0; z < World.chunkSize; z++)
+			for(int y = 0; y < World.chunkSize; y++)
+				for(int x = 0; x < World.chunkSize; x++)
+				{
+					chunkData[x,y,z].Draw();	
+				}
+		CombineQuads();
+	}
+
+	// Use this for initialization
+	public Chunk (Vector3 position, Material c) {
+		
+		chunk = new GameObject(World.BuildChunkName(position));
+		chunk.transform.position = position;
+		cubeMaterial = c;
+		BuildChunk();
+	}
+	
+	void CombineQuads()
+	{
+		//1. Combine all children meshes
+		MeshFilter[] meshFilters = chunk.GetComponentsInChildren<MeshFilter>();
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
         int i = 0;
-        while (i < meshFilters.Length)
-        {
+        while (i < meshFilters.Length) {
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
             i++;
         }
 
-        //Create a new mesh on the parent object
-        MeshFilter mf = (MeshFilter)this.gameObject.AddComponent(typeof(MeshFilter));
+        //2. Create a new mesh on the parent object
+        MeshFilter mf = (MeshFilter) chunk.gameObject.AddComponent(typeof(MeshFilter));
         mf.mesh = new Mesh();
 
-        //Add combines meshes on children as the parent's mesh
+        //3. Add combined meshes on children as the parent's mesh
         mf.mesh.CombineMeshes(combine);
 
-        //Create Renderer for the parent
-        MeshRenderer renderer = this.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
-        renderer.material = cubeMaterial;
+        //4. Create a renderer for the parent
+		MeshRenderer renderer = chunk.gameObject.AddComponent(typeof(MeshRenderer)) as MeshRenderer;
+		renderer.material = cubeMaterial;
 
-        //Delete the uncombined quads
-        foreach (Transform quad in this.transform)
-        {
-            Destroy(quad.gameObject);
-        }
-    }
+		//5. Delete all uncombined children
+		foreach (Transform quad in chunk.transform) {
+     		GameObject.Destroy(quad.gameObject);
+ 		}
 
-    private void Start()
-    {
-        StartCoroutine(BuildChunk(4, 4, 4));
-    }
+	}
+
 }
